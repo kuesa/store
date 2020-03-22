041a84c6e7d67a54165cb180912c05986e67c568
Noah <32714062+kuesa@users.noreply.github.com>
2020-03-22 14:08:34 -0700

    Added API Key comment
    to assuage fears of me publishing secrets.

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
 
+// don't worry - this is a public API key.
 class Checkout extends React.Component {
     render() {
         return (
