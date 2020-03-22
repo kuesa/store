31f4dbefcea431df1c243155c1ea3a4a2a6736d3
kuesa <me@noahgrove.me>
2020-03-22 14:07:32 -0700

    Changed from double to single quotes

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,7 +5,7 @@ import CheckoutForm from './CheckoutForm';
 class Checkout extends React.Component {
     render() {
         return (
-            <StripeProvider apiKey="pk_test_H11hfDxxxCmMMMQWwDGV7bXy00wOk4eBK3">
+            <StripeProvider apiKey='pk_test_H11hfDxxxCmMMMQWwDGV7bXy00wOk4eBK3'>
                 <>
                     <Elements>
                         <CheckoutForm items={this.props.items} />
